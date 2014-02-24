05f4099bda2ddbfcb51ccc3ac253cc3337a240f0
Michael Von Hippel <vonhippelscienceandenterprises@gmail.com>
2014-02-23 23:31:54 -0800

    Trying to copy some files over from kilobolt project
    implementing what I can, but its not really functional at all, just a
    start at hashing things out

--- src/com/decks/Assets.java
+++ src/com/decks/Assets.java
@@ -1,11 +1,22 @@
 package com.decks;
 
+import java.util.*;
+
 import com.decks.framework.Image;
 import com.decks.framework.Sound;
+import com.decks.SampleGame;
 
 public class Assets {
 	
-	public static Image menu;
-	public static Sound click;
+	public static Image menu;					// Also from kilobolt menu system
+	public static Sound click;					// From kilobolt
+	public Set<Player> players;					// All players in game
+	public ArrayList<Deck> decks; 				// All decks in play
+	public ArrayList<DiscardPile> discardPiles; // All DiscardPiles in play
+	public PlaySpace playSpace; 				// PlaySpace where players play cards
+	public Board board; 						// Board where players cannot play cards
 
+	public static void load(SampleGame sampleGame) {
+	}
+	
 }
---
+++ src/com/decks/SplashLoadingScreen.java
@@ -0,0 +1,51 @@
+package com.decks;
+
+import com.decks.framework.*;
+
+public class SplashLoadingScreen extends Screen {
+	public SplashLoadingScreen(Game game) {
+		super(game);
+	}
+
+	@Override
+	public void update(float deltaTime) {
+		// TODO Auto-generated method stub
+		Graphics g = game.getGraphics();
+		Assets.splash= g.newImage("splash.jpg", ImageFormat.RGB565);
+
+		
+		game.setScreen(new LoadingScreen(game));
+
+	}
+
+	@Override
+	public void paint(float deltaTime) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void backButton() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
