0399a164c5ceb64b01064c35e5769c5612990df8
Michael Von Hippel <vonhippelscienceandenterprises@gmail.com>
2014-04-13 00:45:21 -0700

    main menu stuff

--- src/com/decks/MainMenuScreen.java
+++ src/com/decks/MainMenuScreen.java
@@ -27,7 +27,7 @@ public class MainMenuScreen extends Screen {
 
                 if (inBounds(event, 0, 0, 250, 250)) {
                     //START GAME
-                                game.setScreen(new GameScreen(game));               
+                                game.setScreen(new GameScreen(game, null, null, null, null, null));               
       }
 
 
